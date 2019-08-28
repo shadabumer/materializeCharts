c692eaeb4db752966f3aa42dab9584844be2d22a
Shadab Umer <shadabum3r@gmail.com>
2019-08-28 18:16:55 +0530

    first pie chart rendered successfully

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HighchartsChartModule } from 'highcharts-angular';
 
 import { AppComponent } from './app.component';
+import { PieChartComponent } from './pie-chart/pie-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PieChartComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HighchartsChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pie-chart/pie-chart.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
+
+
+@Component({
+  selector: 'app-pie-chart',
+  templateUrl: './pie-chart.component.html',
+  styleUrls: ['./pie-chart.component.css']
+})
+export class PieChartComponent implements OnInit {
+
+  highcharts = Highcharts;
+  chartOptions = {   
+    chart: {
+      plotBackgroundColor: null,
+      plotBorderWidth: null,
+      plotShadow: false,
+      type: 'pie'
+    },
+    title: {
+        text: 'Browser market shares in January, 2018'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: false
+            },
+            showInLegend: true
+        }
+    },
+    series: [{
+      name: 'Brands',
+      colorByPoint: true,
+      data: [{
+          name: 'Chrome',
+          y: 61.41,
+          sliced: true,
+          selected: true
+      }, {
+          name: 'Internet Explorer',
+          y: 11.84
+      }, {
+          name: 'Firefox',
+          y: 10.85
+      }, {
+          name: 'Edge',
+          y: 4.67
+      }, {
+          name: 'Safari',
+          y: 4.18
+      }, {
+          name: 'Other',
+          y: 7.05
+      }]
+    }]
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
